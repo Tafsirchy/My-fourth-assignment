0d7b0b99fd42f0549150072e9caee6c7c4286d70
Tafsirchy <tafsirchy1000@gmail.com>
2025-08-14 17:32:57 +0600

    problem 1

--- problem1.js
+++ problem1.js
@@ -1,1 +1,13 @@
-const number = 100;
+
+function totalFine( fare ) {
+    if(fare <= 0 || typeof fare !== 'number'){
+        return 'Invalid';
+    }
+
+    const fine = fare * (20/100);
+    const extraFine = 30;
+    const total = fare + fine + extraFine;
+    return total;
+}
+
+console.log(totalFine("Gorib tai ticket katinai")); // 130
