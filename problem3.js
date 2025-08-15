d4a7d822953aabdf69243d4c99a596de52cefe86
Tafsirchy <tafsirchy1000@gmail.com>
2025-08-15 11:29:54 +0600

    problem 3 & 4

---
+++ problem3.js
@@ -0,0 +1,24 @@
+function  bestTeam( player1, player2 ) {
+    if(typeof player1 !== "object" || typeof player2 !== "object" || player1 === null || player2 === null){
+        return 'Invalid';
+    }
+
+    const team1 = player1.foul + player1.cardY + player1.cardR;
+
+    const team2 = player2.foul + player2.cardY + player2.cardR;
+
+    if (team1 < team2){
+        return player1.name;
+    }
+    else if (team1 > team2){
+        return player2.name;
+    }
+    else{
+        return 'Tie'
+    }
+    
+}
+
+
+console.log(bestTeam({ name: "Germany", foul: 10, cardY: 1, cardR: 1 },
+{ name: "France", foul: 10, cardY: 2, cardR: 1 }));
---
+++ problem4.js
@@ -0,0 +1,19 @@
+function  isSame(arr1 , arr2 ) {
+    if(!Array.isArray(arr1) || !Array.isArray(arr2)){
+        return 'Invalid';
+    }
+    else if(arr1.length !== arr2.length){
+        return false;
+    }
+    for (let i = 0 ; i < arr1.length ; i++){
+        if(arr1[i] !== arr2[i]){
+            return false;
+        }
+    }
+    return true;
+    
+}
+
+
+const test = isSame([1, 2, 3] , [1 , 2 , 3]);
+console.log(test);
